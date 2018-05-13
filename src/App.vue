6c360f9e6e29ed8ee963fd15276b9b42ccbed349
peterpanBest <pfs309wk@126.com>
2018-05-14 00:04:03 +0800

    5-14

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  beforeCreate(){
+    this.$router.push({name: 'login',params: {"token":"success"}});
+  },
+  created(){
+     this.$router.push({name: 'login',params: {"token":"success"}});
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <el-button>默认按钮</el-button>
+  </div>
+</template>
+
+<script>
+import { Button, Select } from 'element-ui'
+export default {
+  name: '',
+  components: {
+      Button,
+      Select 
+  },
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
